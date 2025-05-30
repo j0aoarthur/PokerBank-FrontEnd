fa9c9437b4ecc7cf57c979b9f770a921897e2389
joaoarthur <arthur1081@hotmail.com>
2025-05-30 11:00:23 -0300

    refactor: Changed NavigationBar to add admin page

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -3,6 +3,7 @@ import {PiRanking} from "react-icons/pi";
 import {GiPokerHand} from "react-icons/gi";
 import {BottomBarContainer, IconWrapper} from "./styles.js";
 import {useNavigate} from "react-router-dom";
+import {RiAdminLine} from "react-icons/ri";
 
 export function NavigationBar({ activePage }) {
     const navigate = useNavigate();
@@ -17,6 +18,9 @@ export function NavigationBar({ activePage }) {
             </IconWrapper>
             <IconWrapper $active={activePage === 'ranking'} onClick={() => navigate("/ranking")}>
                 <PiRanking />
+            </IconWrapper>
+            <IconWrapper $active={activePage === 'admin'} onClick={() => navigate("/admin")}>
+                <RiAdminLine />
             </IconWrapper>
             <IconWrapper $active={activePage === 'logout'} onClick={() => navigate("/")}>
                 <FiLogOut />
