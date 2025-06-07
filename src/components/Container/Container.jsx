5a9fdcf186c3d98a98d7b19ba82c9c04915df22b
joaoarthur <arthur1081@hotmail.com>
2025-06-06 23:15:56 -0300

    refactor: Changed the design of the Container and added a Footer

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,10 +1,38 @@
-import {Outlet} from "react-router-dom";
-import {ContainerWrapper} from "./styles.jsx";
+import {Outlet, useLocation} from "react-router-dom";
+import {ContainerWrapper} from "./styles.js";
+import {NavigationBar} from "../NavigationBar/NavigationBar.jsx";
+import {Footer} from "../Footer/Footer.jsx";
+
+const navItemsForLookup = [
+    { id: 'home', href: '/' },
+    { id: 'games', href: '/games' },
+    { id: 'ranking', href: '/ranking' },
+    { id: 'admin', href: '/admin'},
+];
 
 export function Container() {
+    const location = useLocation();
+    const currentPath = location.pathname;
+
+    let activePageId = '';
+
+    const activeNavItem = navItemsForLookup.find(item => {
+        if (item.href === '/') {
+            return currentPath === item.href;
+        }
+        return currentPath.startsWith(item.href);
+    });
+
+    if (activeNavItem) {
+        activePageId = activeNavItem.id;
+    }
+
     return (
         <ContainerWrapper>
             <Outlet/>
+            <Footer>
+                <NavigationBar activePage={activePageId} />
+            </Footer>
         </ContainerWrapper>
-    )
+    );
 }
---
+++ src/components/Footer/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledFooter = styled.footer`
+    position: sticky;
+    width: 100%;
+    bottom: 0;
+    z-index: 10;
+    background-color: rgba(255, 255, 255, 0.8);
+    backdrop-filter: blur(12px);
+    gap: var(--space-2);
+`
