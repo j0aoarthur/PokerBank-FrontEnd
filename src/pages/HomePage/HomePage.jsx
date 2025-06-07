7d5ca89f5a4e4a7e09ddc2f16645b7655c5a35cf
joaoarthur <arthur1081@hotmail.com>
2025-06-07 15:04:46 -0300

    refactor: removed unused constant

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,18 +2,16 @@ import {useTitle} from "../../utils/useTitle.js";
 import {HomePageContainer} from "./styles.js";
 import {RankingSection} from "../../components/RankingSection/RankingSection.jsx";
 import {GameSection} from "../../components/GameSection/GameSection.jsx";
-import {useEffect, useState} from "react";
 import {HomeHeader} from "../../components/HomeHeader/HomeHeader.jsx";
 import {WelcomeSection} from "../../components/WelcomeSection/WelcomeSection.jsx";
 
 export function HomePage({title}) {
-    const [userName, setUserName] = useState("Usuário");
     useTitle(title)
 
     return (
         <HomePageContainer>
             <HomeHeader/>
-            <WelcomeSection userName={userName} />
+            <WelcomeSection userName={"Usuário"} />
             <GameSection />
             <RankingSection/>
         </HomePageContainer>
